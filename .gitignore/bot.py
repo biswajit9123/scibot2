c9087a443aaeefd382a9a1ee534c7caad2bbc1a3
BestCoderz <utk.bkkg@gmail.com>
2018-11-29 13:58:23 +0530

    Update bot.py

--- .gitignore/bot.py
+++ .gitignore/bot.py
@@ -20,6 +20,52 @@ async def on_ready():
  
  
 @client.event
+async def on_member_join(member):
+    with open('users.json', 'r') as f:
+        users = json.load(f)
+    
+    await update_data(users, member)
+    
+    with open ('users.json', 'w') as f:
+        json.dump(users, f)
+  
+@client.event
+async def on_message(message):
+    with open('users.json', 'r') as f:
+        users = json.load(f)
+        
+    await update_data(users, message.author)
+    await add_experience(users, message.author, 5)
+    await level_up(users, message.author, message.channel)
+                        
+    
+    with open ('users.json', 'w') as f:
+        json.dump(users, f)
+        
+async def update_data(users, user):
+    if not user.id in users:
+        users[user.id] = {}
+        users[user.id]['experience'] = 0
+        users[user.id]['level'] = 1
+    
+async def add_experience(users, user, exp):
+    users[user.id]['experience'] += exp
+    
+async def level_up(users, user, channel):
+    experience = users[user.id]['experience']
+    lvl_start = users[user.id]['level']
+    lvl_end = int(experience ** (1/4))
+    
+    if lvl_start < lvl_end:
+        r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
+        embed = discord.Embed(color = discord.Color((r << 16) + (g << 8) + b))
+        embed.set_author(name='Level Up!')
+        embed.add_field(name = 'User: ',value ='{}'.format(user.name),inline = False)
+        embed.add_field(name = 'Level:',value ='{}'.format(lvl_end),inline = False)
+        await client.send_message(channel, embed=embed)
+        users[user.id]['level'] = lvl_end  
+ 
+@client.event
 async def on_message_edit(before, after):
     if before.content == after.content:
       return
