173b66fe25600dd1d892d599877ef604a7e03990
BestCoderz <utk.bkkg@gmail.com>
2019-01-28 18:35:25 +0530

    Update bot.py

--- .gitignore/bot.py
+++ .gitignore/bot.py
@@ -4,6 +4,7 @@ from discord.ext.commands import Bot
 import platform
 import time
 import os
+from google.cloud import translate
 import colorsys
 import random
 import json
@@ -11,6 +12,11 @@ import json
 client = Bot(description="MyBot is best", command_prefix="mv!", pm_help = False)
 client.remove_command('help')
 
+keyfile = open("keys.txt", "r")
+key = keyfile.read(60)
+
+global last_message
+last_message = "This is a placeholder"
 
 @client.event
 async def on_ready():
@@ -115,6 +121,8 @@ async def on_reaction_remove(reaction, user):
  
 @client.event
 async def on_message(message):
+    global last_message
+    content = message.content
     user = message.author
     if message.author.bot:
       return
@@ -123,6 +131,52 @@ async def on_message(message):
     if message.content.startswith('mv!say'):
       return
     else:
+      if content.startswith("mv!translate"):
+        if " " in content:
+          # When the user wants to call for help, or translate to more than english, this will parse their intention
+          args = content.split(" ", 1)[1]
+          langcode = check_language(args,'language')
+          if args.lower() == "help":
+            help_message = "Type **.translate** to translate the message above to english.\n" \
+                           " Type **.translate <language>** to translate it to that language."
+            await client.send_message(message.channel, help_message)
+          elif langcode != "False":
+            translation = translate_message(last_message, langcode)
+            formatted_return = format_response(translation)
+            await discord_client.send_message(message.channel, formatted_return)
+      else:
+        translation = translate_message(last_message, "en")
+        formatted_return = format_response(translation)
+        await client.send_message(message.channel, formatted_return)
+
+    # Sets this message as the "ast message sent". Keep this at the END of the method
+      last_message = content
+     
+# Translates the message and returns the output object.
+def translate_message(message, target):
+    translate_client = translate.Client.from_service_account_json('api-key.json')
+    result = translate_client.translate(message, target_language=target)
+    return result
+
+
+# Takes in a string that may or may not be a language code.
+# If it is a language code, it returns said code.
+# If it is a language, it returns the affiliated code. If not, it returns -1
+def check_language(code, target):
+    with open('languages.json') as data:
+        languages = json.load(data)
+        for lang in languages:
+            if (lang['language'].lower() == code.lower())or (lang['name'].lower() == code.lower()):
+                return lang[target]
+        return "False"
+
+
+def format_response(response):
+    language_code = response['detectedSourceLanguage']
+    language_name = check_language(language_code, 'name')
+    formatted_return = ("```" + "\n" + " \"" + response['translatedText'] + "\"\n" + "```" + "\n" +
+                        "**Source Language:** " + language_name + " **|** " + language_code + "\n")
+    return formatted_return
       if message.content.startswith('mv!donate'):
           msg = '**Support us by donating us on PayPal:** https://www.paypal.me/RVerma181\n**Support us by donating us on Patreon:** https://www.patreon.com/multiverseofficial'
           await client.send_message(message.channel, msg)
